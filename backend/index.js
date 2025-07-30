c450f14fc31cd207c233a097dab05451da72cbb1
JamesTanakrit <88365807+JamesTanakrit@users.noreply.github.com>
2025-07-30 22:47:03 +0700

    update

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,24 @@
-import express from "express";
 import bodyParser from "body-parser";
+import express from "express";
 import cors from "cors";
 
 const app = express();
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
 const port = 3000;
 
-app.get("/data", (req, res) => {
-  res.send("Welcome to the backend server!");
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  console.log("conecting frontend");
+  res.send({ message: "Connected successfully!" });
 });
 
-app.post("/add-data", (req, res) => {
-  const newData = req.body.data;
-  console.log("Received data:", newData);
-  res.send(`Data received: ${newData}`);
+app.post("/submit", (req, res) => {
+  console.log(req.body);
+  res.send({ message: "Data received successfully!" });
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`server is running on port ${port}`);
 });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,48 +1,121 @@
 import React, { useState } from "react";
 import "./App.css";
-import Header from "./components/Header";
-import FormData from "./components/FormData";
-import Footer from "./components/Footer";
+import axios from "axios";
 
 function App() {
-  const [formData, setFormData] = useState({
+  const [user, setUser] = useState({
     firstname: "",
     lastname: "",
     age: "",
   });
+  // const [inputData, setInputData] = useState("");
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-  };
+  function handleChange(event) {
+    const { name, value } = event.target;
+    // console.log(name);
+    // console.log(value);
+    setUser((prev) => {
+      // console.log(prev);
+      return { ...prev, [name]: value };
+    });
+  }
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Form Data Submitted:", formData);
-    setFormData({
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const age = parseInt(user.age, 10);
+
+    // ตรวจสอบข้อมูล
+    if (!user.firstname || !user.lastname || !user.age) {
+      alert("Please fill in all fields.");
+      return;
+    }
+
+    if (
+      typeof user.firstname !== "string" ||
+      typeof user.lastname !== "string"
+    ) {
+      alert("Please fill firstname and lastname with valid strings.");
+      return;
+    }
+
+    if (isNaN(age)) {
+      alert("Please fill age with a valid number.");
+      return;
+    }
+
+    if (!user.firstname || !user.lastname || !user.age) {
+      alert("Please fill in all fields.");
+      return;
+    } else if (
+      typeof user.firstname !== "string" ||
+      typeof user.lastname !== "string"
+    ) {
+      alert("Please fill firstname with string.");
+      return;
+    } else if (isNaN(age)) {
+      alert("Please fill age with a valid number.");
+      return;
+    }
+    const response = await axios.post("http://localhost:3000/submit", user);
+    console.log(response.data.message);
+    setUser({
       firstname: "",
       lastname: "",
       age: "",
     });
-    // สามารถทำการส่งข้อมูลไปยัง Backend ได้ที่นี่
-  };
+  }
 
   return (
-    <div className="min-h-screen flex flex-col">
-      <Header />
+    <div>
+      <section className="flex flex-row justify-center h-10 items-center my-4">
+        <h1 className="text-xl font-bold underline decoration-solid">
+          Please give us your info
+        </h1>
+      </section>
+      <section className="flex flex-col items-center">
+        <form
+          className="flex flex-col bg-amber-100 p-4 gap-2 rounded-xl border shadow-xl"
+          onSubmit={handleSubmit}
+        >
+          <label htmlFor="firstname">Firstname:</label>
+          <input
+            className="bg-white rounded-full py-1 px-6 border"
+            onChange={handleChange}
+            id="firstname"
+            name="firstname"
+            type="text"
+            placeholder="Firstname"
+            value={user.firstname}
+          />
+          <label htmlFor="lastname">lastname:</label>
+          <input
+            className="bg-white rounded-full py-1 px-6 border"
+            onChange={handleChange}
+            id="lastname"
+            name="lastname"
+            type="text"
+            placeholder="Lastname"
+            value={user.lastname}
+          />
+          <label htmlFor="age">age:</label>
 
-      <div className="flex justify-center items-center flex-grow shadow-xl">
-        <FormData
-          formData={formData} // ส่ง state formData ไปที่ FormData
-          handleChange={handleChange} // ส่ง handleChange ไปที่ FormData
-          handleSubmit={handleSubmit} // ส่ง handleSubmit ไปที่ FormData
-        />
-      </div>
-
-      <Footer />
+          <input
+            className="bg-white rounded-full py-1 px-6 border"
+            onChange={handleChange}
+            id="age"
+            name="age"
+            type="text"
+            placeholder="age"
+            value={user.age}
+          />
+          <button
+            className="bg-blue-400 rounded-full py-1 mx-10 mt-4 mb-1 hover:bg-blue-600 border"
+            type="submit"
+          >
+            submit
+          </button>
+        </form>
+      </section>
     </div>
   );
 }
